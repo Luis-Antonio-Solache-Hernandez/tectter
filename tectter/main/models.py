6ce990d07a5f5cd7cee17ec9a78115d688949b3c
Janis <jis_malaj@hotmail.com>
2012-05-16 21:08:07 -0500

    commit 9

--- tectter/main/models.py
+++ tectter/main/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Perfil(models.Model):
     user = models.OneToOneField(User)
+    name = models.CharField(max_length=30)
     image = models.ImageField(upload_to='img', default='img/User.png')
     birth_date = models.DateField(auto_now=True)
     city = models.CharField(max_length=50, blank=True, null=True)
@@ -11,10 +12,13 @@ class Perfil(models.Model):
     biography = models.TextField(max_length=50, blank=True, null=True)
     friend = models.ManyToManyField('self', blank=True)
 
+    def __unicode__(self):
+        return '%s' % self.user.username
+
 
 def get_profile(user):
     if not hasattr(user, '_profile_cache'):
-        profile, created = Perfil.objects.get_or_create(user=user)
+        profile, created = Perfil.objects.get_or_create(user=user, name=user.username)
         user._profile_cache = profile
     return user._profile_cache
 User.get_profile = get_profile
@@ -24,3 +28,6 @@ class Tweet(models.Model):
     name = models.ForeignKey("Perfil", related_name='tweet')
     status = models.CharField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '%s' % self.status

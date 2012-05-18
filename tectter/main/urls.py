c489d96fe41fe126f6913506223cd0f00e0f2d21
Janis <jis_malaj@hotmail.com>
2012-05-17 21:17:12 -0500

    commit 12

--- tectter/main/urls.py
+++ tectter/main/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('main.views',
     url(r'^$', 'index', name='index'),
     url(r'^accounts/login/$', 'login', name='login'),
-    url(r'^twitt/add/$', 'add_twitt', name='add_twitt'),
     url(r'^perfil/(?P<username>[a-zA-Z0-9\\_\\.]+)/$', 'show_perfil', name='show_perfil'),
     url(r'^perfil/(?P<username>[a-zA-Z0-9\\_\\.]+)/edit/$', 'edit_perfil', name='edit'),
     url(r'^seguir/$', 'seguir'),
